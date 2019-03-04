8a0d00152bf396167802970f6d51148b6273bd3c
zdrummon <zdrummon@gmail.com>
2019-03-04 02:53:30 -0800

    replaced jade with handlebars, updated relevant configurations, ran template tests

--- friendfighter2/routes/index.js
+++ friendfighter2/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Zax party', cheeseArray: ['gouda', 'swiss', 'havarti'], booleanTest: true});
 });
 
 module.exports = router;
