2cabc35d2df9717353ceb8d8aea7b22827809401
zdrummon <zdrummon@gmail.com>
2019-03-03 19:01:06 -0800

    express project started, gitignore added, archive created

--- server.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-var serverRouter = require('./serverRouter');
-
-console.log('server started, waiting for requests');
-http.createServer(serverRouter.handleRequest).listen(8000);
