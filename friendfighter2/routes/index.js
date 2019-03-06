7120b939ca0e07b661015a7830743499b64e9970
zdrummon <zdrummon@gmail.com>
2019-03-06 02:08:23 -0800

    express GET request test success

--- friendfighter2/routes/index.js
+++ friendfighter2/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Zax party', cheeseArray: ['gouda', 'swiss', 'havarti'], booleanTest: true});
 });
 
+router.get('/tests/:id', function(req, res, next) {
+  res.render('test', {output: req.params.id});
+});
+
 module.exports = router;
