7f69780af2f54bc5be3a5e9389697bb89b1378d5
zdrummon <zdrummon@gmail.com>
2019-03-07 06:02:30 -0800

    added session, sessions errors, and session validation

--- friendfighter2/app.js
+++ friendfighter2/app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,12 +18,13 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
  
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- friendfighter2/routes/index.js
+++ friendfighter2/routes/index.js
@@ -3,16 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Zax party', cheeseArray: ['gouda', 'swiss', 'havarti'], booleanTest: true});
+  res.render('index', {title: 'Form Validation', success: req.session.success, errors: req.session.errors});
+  req.session.errors = null;
+  req.session.success = null;
 });
 
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', {output: req.params.id});
-});
+router.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Invalid password').isLength({min: 4}).equals(req.body.confirmPassword);
 
-router.post('/test/submit', function(req, res, next) {
-  var  id = req.body.id
-  res.redirect('/test/' + id);
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
