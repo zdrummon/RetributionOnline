29e5a03f74aa1ec68b3a20b8c92be1823dcd0daa
zdrummon <zdrummon@gmail.com>
2019-03-06 18:58:23 -0800

    testing conflict resolution from zdrummon side

--- friendfighter2/routes/index.js
+++ friendfighter2/routes/index.js
@@ -10,4 +10,9 @@ router.get('/test/:id', function(req, res, next) {
   res.render('test', {output: req.params.id});
 });
 
+router.post('/test/submit', function(req, res, next) {
+  var  id = req.body.id
+  res.redirect('/test/' + id);
+});
+
 module.exports = router;
