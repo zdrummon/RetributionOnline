3616010d6d233525e60b637ccbb7a96740305e7e
Trevor Haggerty <cesismalon@gmail.com>
2019-03-13 18:06:52 -0700

    using the server.js file to return an html file to port 8000

--- trevorsmini/server.js
+++ trevorsmini/server.js
@@ -1,15 +1,23 @@
-console.log ('created a server at port 8000')
+console.log ('created a server at port 8000');
 var http = require('http');
-var map = require('./map')
-var tile = require('./tile')
+var map = require('./map');
+var tile = require('./tile');
+var fs = require('fs');
+
 
 function onRequest(request, response) {
-    console.log ('local host 8000 is activated');
-    response.writeHead(200, {'Content-type': 'text/plain'});
-    response.write('Hello Zac!');
-    map.createMap();
-    tile.createTile();
-    response.end();
+    console.log ('local host 8000 is accessed');
+    response.writeHead(200, {'Content-type': 'text/html'});
+    fs.readFile('./index.html', null, function(error,data){
+        if(error){
+            response.writeHead(404)
+            response.write('file not found')
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+ 
 }
 
 http.createServer(onRequest).listen(8000);
