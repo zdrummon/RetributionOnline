f019ac0e3c18fcb75cb19572212420349d99608f
zdrummon <zdrummon@gmail.com>
2019-03-24 21:02:51 -0700

    added keyboard controls to trevors cave explorer

--- trevorsmini/public/javascripts/caveExplorer001.js
+++ trevorsmini/public/javascripts/caveExplorer001.js
@@ -21,6 +21,9 @@ NEbutton.addEventListener('click', NEbuttonfunction);
 SEbutton.addEventListener('click', SEbuttonfunction);
 Sbutton.addEventListener('click', Sbuttonfunction);
 SWbutton.addEventListener('click', SWbuttonfunction);
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 
 var NWbuttonState = false;
 var NbuttonState = false;
@@ -151,8 +154,56 @@ function mouseClicked(event){
 }
 
 //keyboard input---------------------------------------------
-//-----under 
-// ---------construction
+//key press handler
+var NWbuttonState = false;
+var NbuttonState = false;
+var NEbuttonState = false;
+var SEbuttonState = false;
+var SbuttonState = false;
+var SWbuttonState = false;
+
+function keyDownHandler(e) {
+    if(e.keyCode == 87) {      //w key
+        NWbuttonState = true;
+    }
+    else if(e.keyCode == 69) { //e key
+        NbuttonState = true;
+    }
+    else if(e.keyCode == 82) { //r key
+        NEbuttonState = true;
+    }
+    else if(e.keyCode == 70) { //f key
+        SEbuttonState = true;
+    }
+    else if(e.keyCode == 68) { //d key
+        SbuttonState = true;
+    }
+    else if(e.keyCode == 83) { //s key
+        SWbuttonState = true;
+    }
+}
+  
+//key release handler  
+function keyUpHandler(e) {
+    if(e.keyCode == 87) {      //w key
+        NWbuttonState = false;
+    }
+    else if(e.keyCode == 69) { //e key
+        NbuttonState = false;
+    }
+    else if(e.keyCode == 82) { //r key
+        NEbuttonState = false;
+    }
+    else if(e.keyCode == 70) { //f key
+        SEbuttonState = false;
+    }
+    else if(e.keyCode == 68) { //d key
+        SbuttonState = false;
+    }
+    else if(e.keyCode == 83) { //s key
+        SWbuttonState = false;
+    }
+}
 
 //event handling---------------------------------------------------------
 
