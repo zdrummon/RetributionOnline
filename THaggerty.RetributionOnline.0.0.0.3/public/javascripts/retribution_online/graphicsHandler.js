23faeda43634f727ab8329bd91cd7ff6a0bc78ce
Trevor Haggerty <cesismalon@gmail.com>
2019-03-26 03:06:32 -0700

    graphical adjustments for scaling to 9x16

--- THaggerty.RetributionOnline.0.0.0.3/public/javascripts/retribution_online/graphicsHandler.js
+++ THaggerty.RetributionOnline.0.0.0.3/public/javascripts/retribution_online/graphicsHandler.js
@@ -6,23 +6,36 @@ var monkSprite = document.getElementById("monkSprite");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+/*
+var screenCenterX;
+var screenCenterY;
+screenCenterX = window.innerWidth / 2;
+screenCenterY = window.innerHeight / 2;
+*/
+
+function setTo16x9() {
+    if (canvas.width / 9 > canvas.height / 16) {
+        canvas.width = window.innerHeight * 9/16;
+        canvas.height = window.innerHeight;
+        
+    } else {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerWidth * 16/9;
+    }    
+}
+
+var drawScale = canvas.width / 7.5;
+
 function drawHex(drawX, drawY) {
-    ctx.drawImage(testHex, 0, 0, 32, 32, drawX, drawY, canvas.width / 7.5, canvas.height / 10);
+    ctx.drawImage(testHex, 0, 0, 32, 32, drawX, drawY, drawScale, drawScale);
 }
 
 function drawPlayer(drawX, drawY, facing, frameCount) {
     var frame = frameCount % 8;
-    var mobScale = 32;
-    var frameWidth = 99;
-    var frameHeight = 89;
-    if (canvas.width > canvas.height) {
-        mobScale = canvas.height / 4;
-    } else {
-        mobScale = canvas.width / 4;
-    }
-
+    var frameWidth = 100;
+    var frameHeight = 100;
     //what the hell is this scaling (is it the hexes or the guy?)
-    ctx.drawImage(monkSprite, frameWidth * frame, frameHeight * facing, frameWidth, frameHeight, drawX - mobScale / 2.8, drawY - mobScale / 1.6, mobScale, mobScale);
+    ctx.drawImage(monkSprite, frameWidth * frame, frameHeight * facing, frameWidth, frameHeight, drawX - drawScale / 2, drawY - drawScale / 2, drawScale, drawScale);
 }
 
 function fillScreen(){
@@ -33,24 +46,46 @@ function fillScreen(){
     ctx.closePath();
 }
 
+function printData(){
+    var dx = (j * canvas.width / 9) + canvas.width / 18;
+    var dy = (i * canvas.height / 9) + canvas.height / 18;
+    ctx.font = "10px Arial";
+    ctx.fillStyle = "red"; 
+    if (j % 2 == 0){
+        ctx.fillText(j + ', ' + i, dx, dy+ canvas.height / 24);
+    }
+    else{
+        ctx.fillText(j + ', ' + i, dx, dy);
+    }
+}
+
+var firstPass = true;
+
 function updateScreen() {
+    drawScale = canvas.width / 7.5;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    setTo16x9();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     fillScreen();
     for (i = 0; i < 11; i++) {
         for (j = 0;  j < 11; j++){
-            var dx = j * canvas.width / 11;
-            var dy = i * canvas.height / 11;
+            var dx = j * canvas.width / 9.5 - drawScale * 0.75;
+            var dy = i * canvas.height / 16;
+            
             if (gameBoard[i][j] == 1) { 
                 if (j % 2 == 0){
-                    dy = dy + canvas.height / 24;
+                    dy = dy + canvas.height / 32;
                 }
                 drawHex(dx, dy);
+            } else if (gameBoard[i][j] == 2) {
+                drawPlayer(dx, dy, player.facing, frameCounter);
             }
-            
-                drawPlayer(canvas.width / 2, canvas.height / 2, player.facing, frameCounter);
-            
+            if (firstPass) {
+                console.log("an object at " + j + " " + i + " will be drawn with its top left corner at " + dx + " " + dy);
+            }
+            //printData();
         }
     }
+    firstPass = false;
 }
