022e8d5a9450f7b8df2283815c2be74dccb44611
Trevor Haggerty <cesismalon@gmail.com>
2019-04-01 06:00:39 -0700

    git test

--- RetributionOnline/public/javascripts/retribution_online/main.js
+++ RetributionOnline/public/javascripts/retribution_online/main.js
@@ -1,5 +1,9 @@
-function game() {
+//main.js is the launching javascript file for Retribution Online
 
+
+// leave room for inports here
+
+function game() {
     if(frameTimer()){
         updateScreen();
         updateGame();
