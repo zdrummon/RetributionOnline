fbd8adce3e72585ef5ef36602ff1c87771c01125
zdrummon <zdrummon@gmail.com>
2019-04-01 19:30:30 -0700

    fixed some file structure, updated giteignore

---
+++ RetributionOnline/retribution_online/gameEntityHandler.js
@@ -0,0 +1,27 @@
+//gameEntitiyHandler.js contains the game entity object
+// each instance of this object contains coordinates, an id, and type indicator
+function gameEntity(x, y, z, id, type){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.id = id;
+    this.type = type;
+}
+
+//create mob object
+function mob(){}
+// mame variable to hold gameEntity's  prototype
+var gameEntityMobProto = new gameEntity;
+// add new properties to game entity prototype
+gameEntityMobProto.facing;
+gameEntityMobProto.maxHealth;
+gameEntityMobProto.health;
+gameEntityMobProto.maxStamina;
+gameEntityMobProto.stamina;
+gameEntityMobProto.inventory;
+gameEntityMobProto.statusFlags;
+//set mob objects prototype to modified gameEntity's prototype
+mob.prototype = gameEntityMobProto;
+//set mobs gameEntity type to mob
+mob.type='mob';
+
---
+++ RetributionOnline/retribution_online/spriteDrawer.js
@@ -0,0 +1,10 @@
+
+var monkSprite = document.getElementById("monkSprite");
+
+function drawAnimatedSprite(spriteSheet,drawX, drawY, row, frameCount, totalRows, totalColumns) {
+    var column = frameCount % totalColumns;
+    var frameWidth = spriteSheet.width / totalColumns;
+    var frameHeight = spriteSheet.height / totalRows;
+    ctx.drawImage(spriteSheet, frameWidth * column, frameHeight * row, frameWidth, frameHeight, drawX, drawY, drawScale, drawScale);// drawX, drawY - drawScale / 4, drawScale, drawScale);
+}
+
